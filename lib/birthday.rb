68fd4ecc67bbdb43155bc8d23a57aa406a7ce5a1
Liselle Logan <liselle.logan@gmail.com>
2020-09-19 03:45:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,8 @@ def age_appropriate_birthday(birthday)
   birthday.each do |kid, years_old|
     if years_old <= 12
       puts "Happy Birthday #{kid}! You are now #{years_old}!"
+    else
+      puts "You are too old for this."
     end
   end
 end
